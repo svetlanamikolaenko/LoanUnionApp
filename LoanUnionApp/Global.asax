facfa0c39aaf62b511f1dca2a9e7382e42e81cd7
Svitlana <svetlana.mikolaenko@gmail.com>
2019-03-04 20:22:54 +0200

    Add project files.

---
+++ LoanUnionApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoanUnionApp.MvcApplication" Language="C#" %>
---
+++ LoanUnionApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoanUnionApp.Startup))]
+namespace LoanUnionApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
